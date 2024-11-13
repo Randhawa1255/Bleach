fc5346e4f847aa5bf054a196a0adb561d55281df
Randhawa1255 <TaranjotSingh2341@gmail.com>
2024-11-13 12:43:51 +0530

    some minor changes and the typewritter effect added

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const mode = document.getElementById("theme")
 
 const currenttheme = localStorage.getItem("theme")
-if (currenttheme){
+if (currenttheme) {
     document.body.classList.add(currenttheme)
 }
-else{
-document.body.classList.add("darkmode")
+else {
+    document.body.classList.add("darkmode")
 }
-mode.addEventListener("click", ()=>{
-    if(document.body.classList.contains("darkmode")){
+mode.addEventListener("click", () => {
+    if (document.body.classList.contains("darkmode")) {
         document.body.classList.remove("darkmode")
         document.body.classList.add("lightmode")
-        localStorage.setItem("theme","lightmode")
+        localStorage.setItem("theme", "lightmode")
     }
-    else{
+    else {
         document.body.classList.remove("lightmode");
-        document.body.classList.add("darkmode");localStorage.setItem("theme","darkmode")
+        document.body.classList.add("darkmode"); localStorage.setItem("theme", "darkmode")
     }
+})
+
+var typed = new Typed(".autotext", {
+    strings: ["Programmer", "Designer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
 })
